eb3f8f17187901eb9a9521014424d602d591f49b
davco <davfr_cohen@msn.com>
2018-01-08 10:28:33 +0200

    Add EqualLogs, synchronization of thread example on a logfile writing code

---
+++ EqualLogs/EqualLogs.cpp
@@ -0,0 +1,120 @@
+/*
+ * Equal Logs code
+ * Use Mutex to synchronize multiple thread to write in the same log file during WANTED_RUNTIME seconds
+ */
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+#define MAX_INSTANCE 20
+#define THREADCOUNT 2
+#define WANTED_RUNTIME 30
+#define LOG_MSG "Hello World! I'm thread nb %d which slept during %d milliseconds \r\n"
+#define CANT_RELEASE_MUTEX_MSG "Ohhh Grrrr, can't release mutex ?!"
+#define WRITE_FAILED "Terminal failure: Unable to write to file.\n"
+
+HANDLE mutex;
+
+/* Function to write in a file */
+int writeToFile(LPCTSTR logMessage, LPCTSTR logFilePath) {
+    HANDLE fp = CreateFile(logFilePath, FILE_APPEND_DATA, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD writtenBytes = 0;
+    char* message = (char*) logMessage;
+    BOOL bErrorFlag = WriteFile(fp, logMessage, strlen(message), &writtenBytes, NULL);
+    if (FALSE == bErrorFlag)
+    {
+        printf(WRITE_FAILED);
+    }
+    CloseHandle(fp);
+
+    return 0;
+}
+
+/* Synchronization + Write to file */
+BOOL logWrite(LPCTSTR logMessage, LPCTSTR logFilePath) {
+    DWORD dwWaitResult = WaitForSingleObject(mutex, INFINITE);
+
+    switch (dwWaitResult) 
+    {
+        case WAIT_OBJECT_0:
+            __try {
+                writeToFile(logMessage, logFilePath);
+            }
+
+            __finally {
+                if (!ReleaseMutex(mutex))
+                {
+                    printf(CANT_RELEASE_MUTEX_MSG);
+                }
+            }
+            break;
+
+        case WAIT_ABANDONED:
+            return FALSE;
+    }
+}
+
+/* Thread function */
+DWORD WriteToLogFile(LPVOID lpParam) {
+    int threadNb = (int) lpParam;
+    char buffer[1024];
+    while (1) 
+    {
+        srand((unsigned)time(NULL));
+        int rand_sleep = (rand() % 1800) + 200;
+
+        sprintf(buffer, LOG_MSG, threadNb, rand_sleep);
+        logWrite(buffer,
+            "log.txt");
+        Sleep(rand_sleep);
+    }
+    return 0;
+}
+
+/* Main that create the mutex and the thread */
+int main(void) {
+    HANDLE aThread[THREADCOUNT];
+    DWORD ThreadID;
+    int i;
+    srand(time(NULL));
+
+    // Create mutex
+    mutex = CreateMutex(NULL, FALSE, NULL);
+
+    if (mutex == NULL) {
+        printf("CreateMutex error : %d\n", GetLastError());
+        return 1;
+    }
+
+    // Create threads
+    for (i = 0; i < THREADCOUNT; i++)
+    {
+        aThread[i] = CreateThread(
+            NULL,       // default security attributes
+            0,          // default stack size
+            (LPTHREAD_START_ROUTINE)WriteToLogFile,
+            (LPVOID)i,  // thread number as a parameter
+            0,          // default creation flags
+            &ThreadID); // receive thread identifier
+
+        if (aThread[i] == NULL)
+        {
+            printf("CreateThread error: %d\n", GetLastError());
+            return 1;
+        }
+    }
+
+    /* Manage run time */
+    double runtime = WANTED_RUNTIME;
+    clock_t begin = clock();
+    BOOL runtime_passed = FALSE;
+    while (!runtime_passed) {
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+        if (time_spent > runtime)
+            runtime_passed = TRUE;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WinApi
+
+This repository contains some interesting and basic usage of Win32 api.
+
+You'll learn how to use : 
+
+1.  Use **Mutex** : in EqualLogs repository, you will find a good example of synchronization of thread using Mutex. The goal is to write in a same log file. So, you'll see how to handle mutex in order to prevent threads from writing at the same time in this logfile.
+2.  
